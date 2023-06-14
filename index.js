774b2ce86724cf7ea6798d05fac424bca7ff2f40
Ham kemboi <kemboiham3@gmail.com>
2023-06-14 17:30:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Ham kemboi';
+const welcomeMessage = 'Welcome to Flatbook, + ${Ham kemboi}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser.slice(0, 1);
+const shortGreeting = 'Welcome, ${H.slice(0, 1)}!';
